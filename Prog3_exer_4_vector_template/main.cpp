71501fad429dfb9b32d336d9cf23412dbb6644f7
charissaabegail <morales@mail.hs-ulm.de>
2021-01-10 21:26:27 +0100

    fix: insert and remove

--- Prog3_exer_4_vector_template/main.cpp
+++ Prog3_exer_4_vector_template/main.cpp
@@ -16,18 +16,70 @@ int main(int argc, const char * argv[]) {
     
     vectorDouble1[0] = 1.1;
     vectorDouble1[1] = 2.2;
-    vectorDouble1[1] = 3.3;
+    vectorDouble1[2] = 3.3;
     
     double * vectorDouble2 = new double[3];
     
     vectorDouble2[0] = 4.44;
     vectorDouble2[1] = 5.55;
-    vectorDouble2[1] = 6.66;
+    vectorDouble2[2] = 6.66;
     
-    //instantiate vectorTemplate class with constructor that accepts object size
-    vectorTemplate<double> * vt_double = new vectorTemplate<double>(3);
+    //instantiate vectorTemplate class with constructor that accepts object size and vector
+    std::cout << "VECTOR 1:" << std::endl;
+    vectorTemplate<double> * vt_double1 = new vectorTemplate<double>(3,vectorDouble1);
+    vt_double1->display();
     
-    vt_double->display();
+    std::cout << "VECTOR 2:" << std::endl;
+    vectorTemplate<double> * vt_double2 = new vectorTemplate<double>(3,vectorDouble2);
+    vt_double2->display();
+    
+    std::cout << "VECTOR1 = VECTOR2 [overloaded assignment operator]" << std::endl;
+    // copying vt_double2 to vt_double1
+    vt_double1 = vt_double2;
+    vt_double1->display();
+    
+    std::cout << "REMOVE LAST ELEMENT in VECTOR 1" << std::endl;
+   /*
+    vt_double1->remove(1);
+    vt_double1->display(); */
+    
+   /* std::cout << "SWAPS VECTOR1 and VECTOR2" << std::endl;
+    vt_double1->swap(vt_double2); */
+    
+    std::cout << "VECTOR 1 toString:" << std::endl;
+    std::cout << vt_double1->toString() << std::endl;
+    
+    std::cout << "GET THE SIZE VECTOR 1:" << std::endl;
+    std::cout << vt_double1->getSize() << std::endl;
+    
+    
+    std::cout << "GET ELEMENT BY INDEX:" << std::endl;
+    vectorTemplate<double> a = vt_double1[0];
+    std::cout << a << std::endl;
+    
+    
+    
+    
+   
+    
+    std::cout << "ADD VECTOR3 to VECTOR1" << std::endl;
+    double * vectorDouble3 = new double[3];
+    
+    vectorDouble2[0] = 10.99;
+    vectorDouble2[1] = 11.99;
+    vectorDouble2[2] = 12.99;
+    vectorTemplate<double> * vt_double3 = new vectorTemplate<double>(3,vectorDouble3);
+    vt_double3->display();
+    
+    vt_double1->insert(vt_double3,1); //[][insert here][]
+    vt_double1->display(); 
+    
+    
+    
+    
+    
+    
+    
     
     
     
--- Prog3_exer_4_vector_template/vector_template.h
+++ Prog3_exer_4_vector_template/vector_template.h
@@ -15,8 +15,13 @@ template <typename T>
 class vectorTemplate{
 private:
     size_t size;
-    T* vector;
+    T* vectorValues;
     
+    //! overloaded output
+    friend std::ostream &operator<<(std::ostream & os, const vectorTemplate<T>& vector) {
+        os << vector.toString() << std::endl;
+        return os;
+    }
 public:
     //! The class should provide default constructor
     vectorTemplate();
@@ -34,16 +39,12 @@ public:
     void resize(size_t newSize);
     
     //! swap: swaps two vectors (swapping administrative data is enough!! No need to copy all values!)
-    void swap(vectorTemplate<T> &vec1, vectorTemplate<T> &vec2);
+    void swap(vectorTemplate<T> & vec2);
     
-    //! toString: returns a string representation of the vector in the form (elem1, elem2, ..., elemN)
-    friend std::ostream &operator<<(std::ostream &os, const vectorTemplate<T>& vector) {
-        os << vector.toString() << std::endl;
-        return os;
-    }
+    
     
     //! overload index operator
-    T& operator[ ](std::size_t index) const;
+    vectorTemplate<T>& operator[ ](std::size_t index);
     
     //! overload index operator with constant vector
     //const T& operator[ ](std::size_t index) const;     
@@ -64,8 +65,215 @@ public:
     ~vectorTemplate();
     
     //! toString
+    std::string toString() const;
     
     
 };
+
+
+//inline functions
+
+//! The class should provide default constructor
+template <class T>
+vectorTemplate<T>::vectorTemplate(){};
+
+//! Constructor, that assigns a given value to a given number of vector elements.
+template <class T>
+vectorTemplate<T>::vectorTemplate(size_t size){
+    this->size = size;
+    this->vectorValues = new T[this->size];
+    for(size_t i = 0; i <= size; i++){
+        this->vectorValues[i] = this->vectorValues[i]; //initialize default value
+        
+    }
+    
+};
+
+//!In addition, any other needed constructor shall be defined.
+template <class T>
+vectorTemplate<T>::vectorTemplate(size_t size, T* vector){
+    this->size = size;
+    this->vectorValues = new T[size];
+    for(size_t i = 0; i <= size; i++){
+        this->vectorValues[i] = vector[i];
+    }
+}
+
+//!size: returns the number of elements of the vector. const reference
+template <class T>
+const size_t& vectorTemplate<T>::getSize(){
+    return this->size;
+}
+
+//! resize: allows you to dynamically adjust the size of the vector
+template <class T>
+void vectorTemplate<T>::resize(size_t newSize){
+    //!save original size;
+    size_t original_size = this->size;
+    //!save original vectorValues
+    T* original_vectorValues = this->vectorValues;
+    
+    //! dynamically allocate new memory
+    this->vectorValues = new T[newSize];
+    for(size_t i = 0; i <= original_size; i++){
+        this->vectorValues[i] = original_vectorValues[i];
+    }
+    
+    //! release temporary memory
+    delete[] original_vectorValues;
+    
+};
+
+//! swap: swaps two vectors (swapping administrative data is enough!! No need to copy all vectorValues!)
+template <class T>
+void vectorTemplate<T>::swap(vectorTemplate<T> &vec2){
+    //! check if swappable or same size;
+    T temp;
+    if(this->size == vec2.size) {
+        for(size_t i = 0; i <= this->size; i++){
+            temp = this->vectorValues[i];
+            this->vectorValues[i] = vec2[i];
+            vec2[i] = temp;
+        }
+    }
+};
+
+
+//! overload index operator
+template <class T>
+vectorTemplate<T>& vectorTemplate<T>::operator[ ](std::size_t index) {
+    //! check if index is valid
+    if(index >= this->size || index < 0 ) {
+        throw std::out_of_range("index out of range");
+    }
+    
+    return this->vectorValues[index];
+}
+
+
+//! overload index operator with constant vector
+//// for non-const objects: can be used for assignment??
+/*template <class T>
+const T& vectorTemplate<T>::operator[ ](std::size_t index) const{
+    
+    if(index >= this->size || index < 0 ) {
+        std::out_of_range("index out of range");
+    }
+    
+    return this->vectorValues[index];
+} */
+
+//! overload assignment operator
+template <class T>
+vectorTemplate<T> & vectorTemplate<T>::operator=(const vectorTemplate<T> &rhsVec) const{
+    //! assignment operator replaces the contents of an object and not initializing new objects
+    //! check if this is not equal to itself
+        if(this != &rhsVec) {
+            this->size = rhsVec.size;
+            delete[] this->vectorValues;
+            this->vectorValues = new T[this->size];
+            for(size_t i = 0; i < this->size; i++){
+                this->vectorValues[i] =  rhsVec[i];
+            }
+        }
+
+    
+    
+    return *this;
+    
+}
+
+//! overload output opertaor
+template <class T>
+void vectorTemplate<T>::display(){
+    //std::cout << " ----- vector contents ---- \n" << std::endl;
+    for(size_t i = 0; i < this->size; i++){
+        std::cout << this->vectorValues[i] << "\n" << std::endl;
+    }
+    //std::cout << " ----- end of output ------ \n" << std::endl;
+}
+
+//! remove
+template <class T>
+void vectorTemplate<T>::remove(size_t index) {
+    
+    //!  validate index
+    if(index >= this->size || index < 0) {
+        throw std::out_of_range("index is out of range");
+    }
+    //! save original size
+    size_t original_size = this->size;
+    //! save original vectorValues
+    T* original_vectorValues = this->vectorValues;
+    
+    //! allocate new memory
+    this->size -= 1;
+    this->vectorValues = new T[this->size];
+    
+    for(size_t i = 0; i < original_size; i++){
+        if(i != index){
+            this->vectorValues[i] =original_vectorValues[i];
+        }
+    }
+    delete[] original_vectorValues;
+}; //why the value of index is size_t>
+
+//! insert
+template <class T>
+void vectorTemplate<T>::insert(const vectorTemplate<T>& vector, size_t index){
+    //! validate index
+    if(index >= this->size) {
+        //adjust size automatically when there's no space
+        resize(this->size + vector.size);
+    } else if (index < 0 ) {
+        throw std::out_of_range("index is out of range");
+        return;
+    }
+    
+    //save original size
+    size_t original_size = this->size;
+    
+    //resize vector
+    resize(vector.size);
+    
+    //shift to the right
+    for(size_t i = original_size; i >= index;){
+        this->vectorValues[i+vector.size] = this->vectorValues[i];
+        if(i > 0) {
+            --i;
+        } else {
+            break;
+        }
+    }
+    
+    //insert vector to the newly resized array
+    for(size_t i = index, j=0 ; j < vector.size; j++, i++){
+        this->vectorValues[i] = vector.vectorValues[j];
+    }
+};
+
+//! release
+template <class T>
+vectorTemplate<T>::~vectorTemplate(){
+    delete[] this->vectorValues;
+};
+
+//!toString: returns a string representation of the vector in the form (elem1, elem2, ..., elemN)
+template <class T>
+std::string vectorTemplate<T>::toString() const{
+    std::string temp_values_to_output;
+    temp_values_to_output += "(";
+    for(size_t i = 0; i < this->size; i++){
+        
+        if(i == (this->size-1)){
+            temp_values_to_output += std::to_string(this->vectorValues[i]);
+        } else {
+            temp_values_to_output += std::to_string(this->vectorValues[i])+",";
+        }
+    }
+    temp_values_to_output += ")";
+    return temp_values_to_output;
+    
+}
 
 #endif /* vector_template_h */
