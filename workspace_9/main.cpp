1ed096558e7775d16b65976daff152593cf8e5ee
charissaabegail <morales@mail.hs-ulm.de>
2021-01-11 14:24:28 +0100

    Initial Commit

---
+++ workspace_9/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  workspace_9
+//
+//  Created by Charissa Abegail M. Morales on 1/11/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
